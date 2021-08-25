7d465f2f9bcbf5afb4e480dbd6aecbfa83f75682
Pierre-Yves de Müllenheim <pierre-yves.demullenheim@hotmail.fr>
2021-08-25 23:43:51 +0200

    maj function to prepare data

--- R/prepare_dataset.R
+++ R/prepare_dataset.R
@@ -1,13 +1,18 @@
 prepare_dataset <- function(data) {
   
   df <-
-    readActigraph(data) %>%
-    dataCollapser(TS = "TimeStamp", by = 60) %>%
-    mutate(TimeStamp = as.character(TimeStamp),
-           TimeStamp2 = TimeStamp) %>%
+    data %>%
+    mutate(vm = ceiling(sqrt(axis1^2 + axis2^2 + axis3^2))) %>%
+    collapse_epochs_updated(60) %>%
+    mutate(timestamp = as.character(timestamp),
+           TimeStamp2 = timestamp) %>%
     tidyr::separate("TimeStamp2", c("date", "time"), sep = " ") %>%
     mutate(time = hms::as_hms(time)) %>%
-    dplyr::select(TimeStamp, date, time, everything())
+           rename(inclineStanding = inclinestanding, 
+                  inclineSitting = inclinesitting,
+                  inclineLying = inclinelying) %>%
+    dplyr::select(timestamp, date, time, everything()) %>%
+    as.data.frame
   
   return(df)
   
