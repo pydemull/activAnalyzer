602fd921517aebdac29a73c218bebfad8799adb1
Pierre-Yves de Müllenheim <pierre-yves.demullenheim@hotmail.fr>
2022-02-18 00:39:21 +0100

    maj test

--- tests/testthat/test-get_ratio_mvpa_sed_comment.R
+++ tests/testthat/test-get_ratio_mvpa_sed_comment.R
@@ -11,7 +11,7 @@ test_that("Gives correct comment about MPVA/SED ratio", {
   expect_match(comment_ratio_below_en, "the patient likely already has most of the health benefits from their physical behavior")
   expect_match(comment_ratio_above_en, "the patient could get further health benefits by replacing more sedentary time by physical activity time")
   expect_match(comment_ratio_below_fr, "le patient obtient déjà probablement la plupart des bénéfices de santé liés à son comportement physique")
-  expect_match(comment_ratio_above_fr, "le patient pourrait probablement obtenir davantage de bénéfices de santé en replaçant du temps sédentaire par du temps d'activité physique")
+  expect_match(comment_ratio_above_fr, "le patient pourrait probablement obtenir davantage de bénéfices de santé en remplaçant du temps sédentaire par du temps d'activité physique")
 
   
 })
