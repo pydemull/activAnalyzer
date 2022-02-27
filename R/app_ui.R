440d3a05fe03d19852bdc42ddfdcfbdef57c109d
Pierre-Yves de Müllenheim <pierre-yves.demullenheim@hotmail.fr>
2022-02-27 23:47:30 +0100

    updated ui

--- R/app_ui.R
+++ R/app_ui.R
@@ -18,7 +18,7 @@ app_ui <- function(request) {
                 tags$style(".main-header {vertical-align: middle;}"),
                 tags$style(".main-header .logo {vertical-align: middle;}")
         ),
-        title = span(img(src="www/favicon.png", width = 30), "activAnalyzer 0.3.0"), titleWidth = 237
+        title = span(img(src="www/favicon.png", width = 30), "activAnalyzer 0.4.0"), titleWidth = 237
                                       ),
       shinydashboardPlus::dashboardSidebar(
         tags$style(HTML("
