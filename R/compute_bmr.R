0a338a1542acb824eded94483021ba7e6e825a5b
Pierre-Yves de Müllenheim <pierre-yves.demullenheim@hotmail.fr>
2022-10-21 10:55:58 +0200

    added "intersex" and "prefer not to say" categories for sex classification; updated the `compute_bmr()`and `compute_mets()` functions that now use the `dplyr::case_when()` function to provide results (rather than `ifelse()` function).

--- R/compute_bmr.R
+++ R/compute_bmr.R
@@ -14,28 +14,39 @@
 #' @examples
 #' compute_bmr(age = 32, sex = "male", weight = 67)
 #' 
-compute_bmr <- function(age = 40, sex = c("male", "female", "undefined"), weight = 70) {
+compute_bmr <- function(age = 40, sex = c("male", "female", "intersex", "undefined", "prefer not to say"), weight = 70) {
   
 sex <- match.arg(sex)
   
 
 # BMR using sex and weight only (Henry, 2005; doi: 10.1079/PHN2005801)
-bmr <-
-  ifelse(age <3 & sex == "male", 61.0 * weight - 33.7,
-         ifelse(age >=3 & age <10 & sex == "male", 23.3 * weight + 514,
-                ifelse(age >=10 & age <18 & sex == "male", 18.4 * weight + 581,
-                       ifelse(age >=18 & age <30 & sex == "male", 16.0 * weight + 545,
-                              ifelse(age >=30 & age <60 & sex == "male", 14.2  * weight + 593,
-                                     ifelse(age >=60 & age <70 & sex == "male", 13.0 * weight + 567,
-                                            ifelse(age >=70 & sex == "male", 13.7 * weight + 481,
+bmr <- dplyr::case_when(
+
+  age <3   & sex == "male"             ~ 61.0 * weight - 33.7,
+  age >=3  & age <10 & sex == "male"   ~ 23.3 * weight + 514,
+  age >=10 & age <18 & sex == "male"   ~ 18.4 * weight + 581,
+  age >=18 & age <30 & sex == "male"   ~ 16.0 * weight + 545,
+  age >=30 & age <60 & sex == "male"   ~ 14.2  * weight + 593,
+  age >=60 & age <70 & sex == "male"   ~ 13.0 * weight + 567,
+  age >=70 & sex == "male"             ~ 13.7 * weight + 481,
                                                    
-                                                   ifelse(age <3 & sex == "female", 58.9 * weight - 23.1,
-                                                          ifelse(age >=3 & age <10 & (sex == "female" | sex == "undefined"), 20.1 * weight + 507,
-                                                                 ifelse(age >=10 & age <18 & (sex == "female" | sex == "undefined"), 11.1 * weight + 761,
-                                                                        ifelse(age >=18 & age <30 & (sex == "female" | sex == "undefined"), 13.1 * weight + 558,
-                                                                               ifelse(age >=30 & age <60 & (sex == "female" | sex == "undefined"), 9.74 * weight + 694,
-                                                                                      ifelse(age >=60 & age <70 & (sex == "female" | sex == "undefined"), 10.2 * weight +  572,
-                                                                                             ifelse(age >=70 & (sex == "female" | sex == "undefined"), 10.0 * weight + 577))))))))))))))
+  age <3   & sex == "female"                                   ~ 58.9 * weight - 23.1,
+  age >=3  & age <10 & (sex == "female" | sex == "undefined" | sex == "prefer not to say")  ~ 20.1 * weight + 507,
+  age >=10 & age <18 & (sex == "female" | sex == "undefined" | sex == "prefer not to say")  ~ 11.1 * weight + 761,
+  age >=18 & age <30 & (sex == "female" | sex == "undefined" | sex == "prefer not to say")  ~ 13.1 * weight + 558,
+  age >=30 & age <60 & (sex == "female" | sex == "undefined" | sex == "prefer not to say")  ~ 9.74 * weight + 694,
+  age >=60 & age <70 & (sex == "female" | sex == "undefined" | sex == "prefer not to say")  ~  10.2 * weight + 572,
+  age >=70 & (sex == "female" | sex == "undefined" | sex == "prefer not to say")            ~  10.0 * weight + 577,
+  
+  age <3   & sex == "intersex"             ~ ((61.0 * weight - 33.7) + (58.9 * weight - 23.1)) / 2,
+  age >=3  & age <10 & sex == "intersex"   ~ ((23.3 * weight + 514) + (20.1 * weight + 507)) / 2,
+  age >=10 & age <18 & sex == "intersex"   ~ ((18.4 * weight + 581) + (11.1 * weight + 761)) / 2,
+  age >=18 & age <30 & sex == "intersex"   ~ ((16.0 * weight + 545) + (13.1 * weight + 558)) / 2,
+  age >=30 & age <60 & sex == "intersex"   ~ ((14.2  * weight + 593) + (9.74 * weight + 694)) / 2,
+  age >=60 & age <70 & sex == "intersex"   ~ ((13.0 * weight + 567) + (10.2 * weight +  572)) / 2,
+  age >=70 & sex == "intersex"             ~ ((13.7 * weight + 481) + (10.0 * weight + 577)) / 2
+  
+)
 
 # BMR using sex, weight and height (Henry, 2005; doi: 10.1079/PHN2005801)
 
--- tests/testthat/test-compute_mets.R
+++ tests/testthat/test-compute_mets.R
@@ -1,84 +1,120 @@
 
 test_that("METs are correctly computed with Santos-Lozano et al. equation [older adults]", {
   
-  MET_santos_older_man_male <- 2.5878 + 0.00047 * 1500 - 0.6453 * 2 # computation for a male
-  MET_santos_older_man_female <- 2.5878 + 0.00047 * 1500 - 0.6453 * 1 # computation for a female
-  MET_santos_older_man_undefined <- 2.5878 + 0.00047 * 1500 - 0.6453 * 1 # computation for a undefined sex
+  MET_santos_older_male <- 2.5878 + 0.00047 * 1500 - 0.6453 * 2 # computation for a male
+  MET_santos_older_female <- 2.5878 + 0.00047 * 1500 - 0.6453 * 1 # computation for a female
+  MET_santos_older_undefined <- 2.5878 + 0.00047 * 1500 - 0.6453 * 1 # computation for an undefined sex
+  MET_santos_older_intersex <- mean(c(MET_santos_older_male, MET_santos_older_female)) # computation for intersex
   
-  MET_santos_older_func_male <- compute_mets(data = data.frame(vm = 1500), 
+
+  MET_santos_older_func_male <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500), 
                                         equation = "Santos-Lozano et al. (2013) [Older adults]", sex = "male")
-  MET_santos_older_func_female <- compute_mets(data = data.frame(vm = 1500), 
+  MET_santos_older_func_female <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500), 
                                              equation = "Santos-Lozano et al. (2013) [Older adults]", sex = "female")
-  MET_santos_older_func_undefined <- compute_mets(data = data.frame(vm = 1500), 
+  MET_santos_older_func_undefined <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500), 
                                                equation = "Santos-Lozano et al. (2013) [Older adults]", sex = "undefined")
-    
-  expect_equal(MET_santos_older_func_male, MET_santos_older_man_male)
-  expect_equal(MET_santos_older_func_female, MET_santos_older_man_female)
-  expect_equal(MET_santos_older_func_undefined, MET_santos_older_man_undefined)
+  MET_santos_older_func_intersex <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500), 
+                                                  equation = "Santos-Lozano et al. (2013) [Older adults]", sex = "intersex")
+  MET_santos_older_func_no_answer <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500), 
+                                                 equation = "Santos-Lozano et al. (2013) [Older adults]", sex = "prefer not to say")
+  
+  
+  expect_equal(MET_santos_older_func_male, MET_santos_older_male)
+  expect_equal(MET_santos_older_func_female, MET_santos_older_female)
+  expect_equal(MET_santos_older_func_undefined, MET_santos_older_undefined)
+  expect_equal(MET_santos_older_func_intersex, MET_santos_older_intersex)
+  expect_equal(MET_santos_older_func_no_answer, MET_santos_older_female)
   
 })
 
 
 test_that("METs are correctly computed with Santos-Lozano et al. equation [Adults]", {
   
-  MET_santos_adult_man_male <- 2.8323 + 0.00054 * 1500 - 0.05912 * 67 + 1.4410 * 2  # computation for a male
-  MET_santos_adult_man_female <- 2.8323 + 0.00054 * 1500 - 0.05912 * 67 + 1.4410 * 1  # computation for a female
-  MET_santos_adult_man_undefined <- 2.8323 + 0.00054 * 1500 - 0.05912 * 67 + 1.4410 * 1  # computation for undefined sex
+  MET_santos_adult_male <- 2.8323 + 0.00054 * 1500 - 0.05912 * 67 + 1.4410 * 2  # computation for a male
+  MET_santos_adult_female <- 2.8323 + 0.00054 * 1500 - 0.05912 * 67 + 1.4410 * 1  # computation for a female
+  MET_santos_adult_undefined <- 2.8323 + 0.00054 * 1500 - 0.05912 * 67 + 1.4410 * 1  # computation for undefined sex
+  MET_santos_adult_intersex <- mean(c(MET_santos_adult_male, MET_santos_adult_female)) # computation for intersex
   
-  MET_santos_adult_func_male <- compute_mets(data = data.frame(vm = 1500), 
+  MET_santos_adult_func_male <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500),  
                                                equation = "Santos-Lozano et al. (2013) [Adults]", 
                                                weight = 67,
                                                sex = "male")
-  MET_santos_adult_func_female <- compute_mets(data = data.frame(vm = 1500), 
+  MET_santos_adult_func_female <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500), 
                                         equation = "Santos-Lozano et al. (2013) [Adults]", 
                                         weight = 67,
                                         sex = "female")
-  MET_santos_adult_func_undefined<- compute_mets(data = data.frame(vm = 1500), 
+  MET_santos_adult_func_undefined <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500),  
                                                equation = "Santos-Lozano et al. (2013) [Adults]", 
                                                weight = 67,
                                                sex = "undefined")
+  MET_santos_adult_func_intersex <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500),  
+                                                 equation = "Santos-Lozano et al. (2013) [Adults]", 
+                                                 weight = 67,
+                                                 sex = "intersex")
+  MET_santos_adult_func_no_answer <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500),  
+                                                 equation = "Santos-Lozano et al. (2013) [Adults]", 
+                                                 weight = 67,
+                                                 sex = "prefer not to say")
   
-  expect_equal(MET_santos_adult_func_male, MET_santos_adult_man_male)
-  expect_equal(MET_santos_adult_func_female, MET_santos_adult_man_female)
-  expect_equal(MET_santos_adult_func_undefined, MET_santos_adult_man_undefined)
+  expect_equal(MET_santos_adult_func_male, MET_santos_adult_male)
+  expect_equal(MET_santos_adult_func_female, MET_santos_adult_female)
+  expect_equal(MET_santos_adult_func_undefined, MET_santos_adult_undefined)
+  expect_equal(MET_santos_adult_func_intersex, MET_santos_adult_intersex)
+  expect_equal(MET_santos_adult_func_no_answer, MET_santos_adult_female)
   
 })
 
 test_that("METs are correctly computed with Sasaki et al. equation [Adults]", {
   
-  MET_sasaki_adult_man <- 0.668876 + 0.000863 * 1500
-  MET_sasaki_adult_func_male <- compute_mets(data = data.frame(vm = 1500), 
+  MET_sasaki_adult <- 0.668876 + 0.000863 * 1500
+  MET_sasaki_adult_func_male <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500), 
                                         equation = "Sasaki et al. (2011) [Adults]",
                                         sex = "male")
-  MET_sasaki_adult_func_female <- compute_mets(data = data.frame(vm = 1500), 
+  MET_sasaki_adult_func_female <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500),  
                                         equation = "Sasaki et al. (2011) [Adults]",
                                         sex = "female")
-  MET_sasaki_adult_func_undefined <- compute_mets(data = data.frame(vm = 1500), 
+  MET_sasaki_adult_func_undefined <- compute_mets(data =data.frame(axis1 = 1995, vm = 1500),  
                                         equation = "Sasaki et al. (2011) [Adults]",
                                         sex = "undefined")
+  MET_sasaki_adult_func_intersex <- compute_mets(data =data.frame(axis1 = 1995, vm = 1500),  
+                                                  equation = "Sasaki et al. (2011) [Adults]",
+                                                  sex = "intersex")
+  MET_sasaki_adult_func_no_answer <- compute_mets(data =data.frame(axis1 = 1995, vm = 1500),  
+                                                 equation = "Sasaki et al. (2011) [Adults]",
+                                                 sex = "prefer not to say")
   
-  expect_equal(MET_sasaki_adult_func_male, MET_sasaki_adult_man)
-  expect_equal(MET_sasaki_adult_func_female, MET_sasaki_adult_man)
-  expect_equal(MET_sasaki_adult_func_undefined, MET_sasaki_adult_man)
+  expect_equal(MET_sasaki_adult_func_male, MET_sasaki_adult)
+  expect_equal(MET_sasaki_adult_func_female, MET_sasaki_adult)
+  expect_equal(MET_sasaki_adult_func_undefined, MET_sasaki_adult)
+  expect_equal(MET_sasaki_adult_func_intersex, MET_sasaki_adult)
+  expect_equal(MET_sasaki_adult_func_no_answer, MET_sasaki_adult)
   
 })
 
 test_that("METs are correctly computed with Freedson et al. equation [Adults]", {
   
-  MET_freedson_adult_man <-  1.439008 + 0.000795 * 1995 
-  MET_freedson_adult_func_male <- compute_mets(data = data.frame(axis1 = 1995), 
+  MET_freedson_adult <-  1.439008 + 0.000795 * 1995 
+  MET_freedson_adult_func_male <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500),  
                                         equation = "Freedson et al. (1998) [Adults]",
                                         sex = "male")
-  MET_freedson_adult_func_female <- compute_mets(data = data.frame(axis1 = 1995), 
+  MET_freedson_adult_func_female <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500),  
                                           equation = "Freedson et al. (1998) [Adults]",
                                           sex = "female")
-  MET_freedson_adult_func_undefined <- compute_mets(data = data.frame(axis1 = 1995), 
+  MET_freedson_adult_func_undefined <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500), 
                                           equation = "Freedson et al. (1998) [Adults]",
                                         sex = "undefined")
+  MET_freedson_adult_func_intersex <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500), 
+                                                    equation = "Freedson et al. (1998) [Adults]",
+                                                    sex = "intersex")
+  MET_freedson_adult_func_no_answer <- compute_mets(data = data.frame(axis1 = 1995, vm = 1500), 
+                                                   equation = "Freedson et al. (1998) [Adults]",
+                                                   sex = "prefer not to say")
   
-  expect_equal(MET_freedson_adult_func_male, MET_freedson_adult_man)
-  expect_equal(MET_freedson_adult_func_female, MET_freedson_adult_man)
-  expect_equal(MET_freedson_adult_func_undefined, MET_freedson_adult_man)
+  expect_equal(MET_freedson_adult_func_male, MET_freedson_adult)
+  expect_equal(MET_freedson_adult_func_female, MET_freedson_adult)
+  expect_equal(MET_freedson_adult_func_undefined, MET_freedson_adult)
+  expect_equal(MET_freedson_adult_func_intersex, MET_freedson_adult)
+  expect_equal(MET_freedson_adult_func_no_answer, MET_freedson_adult)
 })
 
 
