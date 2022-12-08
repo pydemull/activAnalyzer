93a3f3c1746ef3d4a1582e9feccb622a23c89b59
Pierre-Yves de Müllenheim <pierre-yves.demullenheim@hotmail.fr>
2022-12-08 22:20:44 +0100

    fixed issue #28

--- tests/testthat/test-recap_by_day.R
+++ tests/testthat/test-recap_by_day.R
@@ -35,9 +35,9 @@ test_that("The function provides similar summary by day for appropriate metrics 
 
   # Compute results with 60-s epochs
   file <- system.file("extdata", "acc.agd", package = "activAnalyzer")
-  mydata2 <- prepare_dataset(data = file)
-  mydata2_with_wear_marks <- mark_wear_time(
-    dataset = mydata2, 
+  mydata <- prepare_dataset(data = file)
+  mydata_with_wear_marks <- mark_wear_time(
+    dataset = mydata, 
     TS = "TimeStamp", 
     to_epoch = 60,
     cts  = "vm",
@@ -45,8 +45,8 @@ test_that("The function provides similar summary by day for appropriate metrics 
     allowanceFrame = 2, 
     streamFrame = 30
   )
-  mydata2_with_intensity_marks <- mark_intensity(
-    data = mydata2_with_wear_marks, 
+  mydata_with_intensity_marks <- mark_intensity(
+    data = mydata_with_wear_marks, 
     col_axis = "vm", 
     equation = "Sasaki et al. (2011) [Adults]",
     sed_cutpoint = 200, 
@@ -58,7 +58,7 @@ test_that("The function provides similar summary by day for appropriate metrics 
   )
   recap2 <-
     recap_by_day(
-      data = mydata2_with_intensity_marks, 
+      data = mydata_with_intensity_marks, 
       age = 32, 
       weight = 67, 
       sex = "male",
@@ -69,3 +69,55 @@ test_that("The function provides similar summary by day for appropriate metrics 
   expect_equal(recap1, recap2)
   
 })
+
+
+test_that("Kilocalories are correctly summed when changing the period of the day considered for analysis", {
+  
+  file <- system.file("extdata", "acc.agd", package = "activAnalyzer")
+  mydata <- prepare_dataset(data = file)
+  mydata_with_wear_marks <- mark_wear_time(
+    dataset = mydata, 
+    TS = "TimeStamp", 
+    to_epoch = 60,
+    cts  = "vm",
+    frame = 90, 
+    allowanceFrame = 2, 
+    streamFrame = 30
+  )
+  mydata_with_intensity_marks <- mark_intensity(
+    data = mydata_with_wear_marks, 
+    col_axis = "vm", 
+    equation = "Sasaki et al. (2011) [Adults]",
+    sed_cutpoint = 200, 
+    mpa_cutpoint = 2690, 
+    vpa_cutpoint = 6167, 
+    age = 32,
+    weight = 67,
+    sex = "male",
+  ) %>%
+  dplyr::mutate(
+    kcal = 2 # set dummy data for easier calculations
+  )
+  recap1 <-
+    recap_by_day(
+      data = mydata_with_intensity_marks %>% dplyr::filter(days == 1), 
+      valid_wear_time_start = "10:00:01",
+      valid_wear_time_end = "20:00:00",
+      age = 32, 
+      weight = 67, 
+      sex = "male",
+    ) %>%
+    dplyr::select(total_kcal, pal) %>%
+    as.data.frame()
+  
+  recap2 <-
+    data.frame(
+      total_kcal = 10*60*2,
+      pal = (10*60*2) *(10/9) / (compute_bmr(age = 32, sex = "male", weight = 67) / (24*60) * 10*60)
+    )
+  
+  expect_equal(round(recap1,1), round(recap2,1))
+  
+})
+
+
