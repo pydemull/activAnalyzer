72d5639ff69873af5cde49b84845122d5cb09f55
Pierre-Yves de Müllenheim <pierre-yves.demullenheim@hotmail.fr>
2023-01-02 16:41:16 +0100

    added circles for reference thresholds to radar plots showing MX metrics by day

--- R/create_fig_mx_by_day.R
+++ R/create_fig_mx_by_day.R
@@ -5,6 +5,11 @@
 #'     the \code{\link{recap_by_day}} functions.
 #' @param labels A vector of numeric values setting the breaks of the Y axis of the radar plot. Default is a vector of 6 values with
 #'     a start at 0 and an end at the maximum of all the computed MX metrics.
+#' @param mpa_cutpoint A numeric value at and above which time is considered as spent in moderate-to-vigorous physical activity (in counts/epoch length used to compute MX metrics). 
+#'    Defaut value is from Sasaki et al. (2011; doi:10.1016/j.jsams.2011.04.003) relating to vector magnitude in counts/min.
+#' @param vpa_cutpoint A numeric value at and above which time is considered as spent in vigorous physical activity (in counts/epoch length used to compute MX metrics). 
+#'    Defaut value is from Sasaki et al. (2011; doi:10.1016/j.jsams.2011.04.003) relating to vector magnitude in counts/min.
+#' 
 #'
 #' @return A ggplot object
 #' @export
@@ -46,12 +51,14 @@
 #'     )$df_all_metrics
 #' create_fig_mx_by_day(
 #'     data = summary_by_day,
-#'     labels = seq(0, 10000, 2500)
+#'     labels = seq(2500, 12500, 2500)
 #' )
 
 create_fig_mx_by_day <- function(
     data,
-    labels = NULL
+    labels = NULL,
+    mpa_cutpoint = 2690, 
+    vpa_cutpoint = 6167
 ){
   
 # Tidy dataframe
@@ -76,28 +83,43 @@ if (is.null(labels)) {
 df_labels <-
   data.frame(
     date = as.Date(rep(levels(as.factor(data2$date)), each = length(selected_labels))),
-    x_lab = rep(pi/2.1, nlevels(as.factor(data2$date)) * length(selected_labels)),
+    x_lab = rep(30, nlevels(as.factor(data2$date)) * length(selected_labels)),
     y_lab = rep(selected_labels, nlevels(as.factor(data2$date)))
   )
 
+# Creating a dataframe for circles (thresholds)
+df_circles <-
+  data.frame(x = seq(1, 360, 1)) %>%
+  dplyr::mutate(
+    y_mpa = mpa_cutpoint,
+    y_vpa = vpa_cutpoint
+  ) %>%
+  tidyr::pivot_longer(cols = -x, names_to = "cutpoint", values_to = "val")
+
 # Making plot
 p <-
-  ggplot(data = data2, aes(x = metric, y = val)) +
-  geom_polygon(aes(group = date,  color = as.factor(date)), alpha = 0) +
+ggplot(data = data2, aes(x = rep(seq(0, 300, 60),  nlevels(as.factor(date))), y = val)) +
+  geom_polygon(aes(group = date,  color = as.factor(date), fill =  as.factor(date)), alpha = 0.1) +
+  geom_path(data = df_circles, aes(x = x, y = val, linetype = cutpoint), color = "red") +
   geom_text(
     data = df_labels,
-    aes(x = x_lab, y = y_lab, label = y_lab)
-    ) +
-  coord_radar(start = -pi/6) +
-  scale_y_continuous(breaks = selected_labels, labels = selected_labels) +
-  labs(x = NULL, y = NULL, color = "Date") +
+    aes(x = x_lab, y = y_lab, label = y_lab),
+    size = 3
+  ) +
+  coord_radar(start = 0) +
+  scale_x_continuous(breaks = seq(0, 300, 60), labels = c("M1/3", "M120", "M60", "M30", "M15", "M5")) +
+  scale_y_continuous(breaks = selected_labels, minor_breaks = mpa_cutpoint, labels = selected_labels) +
+  scale_linetype_manual(values = c("dashed", "dotdash"), labels = c("MVPA cutpoint", "VPA cutpoint")) + 
+  labs(x = NULL, y = NULL, linetype = "") +
   theme_bw() +
   theme(
     axis.text.y = element_blank(),
     axis.ticks.y = element_blank(),
-    legend.position = "none"
+    legend.position = "bottom"
   ) +
-  facet_wrap(. ~date, ncol = 4)
+  facet_wrap(. ~date, ncol = 4) +
+  guides(color = "none", fill = "none")
+  
 
 # Returning plot
 return(p)
