93af4ca975ba10ee330475a766100db5a0572984
Pierre-Yves de Müllenheim <pierre-yves.demullenheim@hotmail.fr>
2024-04-29 23:00:47 +0200

    added the option for changing the epoch duration in the internal function 'do_all_analyses()'

--- R/do_all_analyses.R
+++ R/do_all_analyses.R
@@ -8,7 +8,7 @@
 #' @return
 #' A dataset (1 row) with all computed metrics.
 
-do_all_analyses <- function(){
+do_all_analyses <- function(to_epoch = 60){
   
   # Load file
   file <- system.file("extdata", "acc.agd", package = "activAnalyzer")
@@ -17,7 +17,7 @@ do_all_analyses <- function(){
   mydata <- prepare_dataset(data = file)
   
   # Detect nonwear time
-  mydata_with_wear_marks <- mydata %>% mark_wear_time()
+  mydata_with_wear_marks <- mydata %>% mark_wear_time(to_epoch = to_epoch)
   
   # Add intensity marks
   mydata_with_intensity_marks <- mark_intensity(data = mydata_with_wear_marks)
