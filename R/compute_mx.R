4a369577ab66ae5ffbf7a4abb360fbbfccdc051d
Pierre-Yves de Müllenheim <pierre-yves.demullenheim@hotmail.fr>
2024-05-06 00:07:48 +0200

    corrected typos in the documentation

--- R/compute_mx.R
+++ R/compute_mx.R
@@ -1,10 +1,10 @@
 #' Compute MX metric
 #' 
 #' An MX metric is the count/epoch value at and above which a given amount of the
-#'     most active minutes is spent (continuously or discontinuously)
+#'     most active minutes is spent (continuously or discontinuously).
 #'
 #' @param x A vector of counts data.
-#' @param n A integer setting the number of rows in correspondance with the 
+#' @param n An integer setting the number of rows in correspondance with the 
 #'     targetted amount of time.
 #'
 #' @return A numeric value.
